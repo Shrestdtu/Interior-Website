719598f23774bd00c4b023141cda6ce6cab91cee
Shrestdtu <86075036+Shrestdtu@users.noreply.github.com>
2022-05-29 14:15:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("wmore").addEventListener("click",function(){
+    document.getElementById("f").style.backgroundImage="url('hi.jpeg')";
+})
+
+document.getElementById("subss").addEventListener("click",function(){
+    document.getElementById("f").style.backgroundImage= "url('1487187.jpeg')";
+})
